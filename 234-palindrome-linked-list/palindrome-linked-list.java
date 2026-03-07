5407de5150561326e2147863bbed71f23c21b502
Harshil Sharma <harshils3107@gmail.com>
2026-03-07 21:06:42 +0530

    Time: 3 ms (99.73%) | Memory: 94.6 MB (43.71%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,22 +10,47 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        List<Integer> check=new ArrayList<>();
-        List<Integer> check2=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null) {
-            check.add(temp.val);
-            temp=temp.next;
-        }
-
-        for(int i=check.size()-1;i>0;i--) {
-            check2.add(check.get(i));
-        }
-        for(int i=0;i<check.size()-1;i++) {
-            if (check.get(i)!=check2.get(i)) return false;
-        }
-        return true;
-
+        if(head == null || head.next == null) return true;
         
+        ListNode s = head;
+        ListNode f = head;
+        while(f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+        
+        ListNode prev = null;
+        ListNode curr = s;
+        while(curr != null) {
+            ListNode fwd = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = fwd;
+        }
+        
+        ListNode checker = head;
+        ListNode checker2 = prev;
+        
+        while(checker2 != null) {
+            if(checker.val != checker2.val) return false;
+            checker = checker.next;
+            checker2 = checker2.next;
+        }
+        
+        return true;
     }
 }
+        //List<Integer> check=new ArrayList<>();
+        //List<Integer> check2=new ArrayList<>();
+        //ListNode temp=head;
+        //while(temp!=null) {
+        //    check.add(temp.val);
+        //    temp=temp.next;
+        //}
+        //for(int i=check.size()-1;i>0;i--) {
+        //    check2.add(check.get(i));
+        //}
+        //for(int i=0;i<check.size()-1;i++) {
+        //    if (check.get(i)!=check2.get(i)) return false;
+        //}
+        //return true;
