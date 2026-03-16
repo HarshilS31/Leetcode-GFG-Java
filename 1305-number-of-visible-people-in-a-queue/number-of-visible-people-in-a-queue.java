4e1518328f0c85a91cc924cdd16cac3ec15459db
Harshil Sharma <harshils3107@gmail.com>
2026-03-16 19:08:19 +0530

    Time: 53 ms (63.98%) | Memory: 116.4 MB (45.89%) - LeetSync

---
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] canSeePersonsCount(int[] arr) {
+        int n=arr.length;
+        Stack<Integer> s= new Stack<>();
+        s.push(arr[n-1]);
+        int [] ans = new int[n];
+        ans[n-1]=0;
+        for(int i=n-2;i>=0;i--) {
+            int count=0;
+            while(s.size()>0 && s.peek()<=arr[i]) {
+                count++;
+                s.pop();    
+            }
+            if(s.size()>0) count++;//means that we found the next greater element!
+            ans[i]=count;
+            s.push(arr[i]);
+        }
+        return ans;
+
+
+        
+    }
+}
