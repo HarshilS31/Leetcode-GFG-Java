e711f3795630c42c527028d62cce4ab339a0f2ea
Harshil Sharma <harshils3107@gmail.com>
2026-04-01 19:36:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -0,0 +1,43 @@
+class Pair{
+    Node node;
+    int dist;
+    Pair(Node node,int dist) {
+        this.node=node;
+        this.dist=dist;
+    }
+    
+}
+class Solution {
+    
+    public ArrayList<Integer> topView(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        HashMap<Integer,Integer> map= new HashMap<>();//HashMap<Horizontal Distance,Data>
+        Queue<Pair> q= new LinkedList<>();
+        q.add(new Pair(root,0));
+        int minDist=Integer.MAX_VALUE;
+        int maxDist=Integer.MIN_VALUE;
+        
+        while(q.size()>0) {
+            Pair front =q.remove();
+            Node node =front.node;
+            int level=front.dist;
+            minDist=Math.min(level,minDist);
+            maxDist=Math.max(level,maxDist); 
+            if(!map.containsKey(level)) map.put(level,node.data);
+            if(node.left!=null) q.add(new Pair(node.left,level-1));
+            if(node.right!=null) q.add(new Pair(node.right,level+1));
+   
+        }
+        for(int i=minDist;i<=maxDist;i++) {
+            ans.add(map.get(i));
+            
+        }
+        return ans;
+        
+       
+        
+    }
+
+
+    
+}
