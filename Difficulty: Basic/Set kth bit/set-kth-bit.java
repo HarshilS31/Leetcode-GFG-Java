4b61656bfceb3bb12efef1792db56630b0502bf1
Harshil Sharma <harshils3107@gmail.com>
2026-04-05 19:18:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Set kth bit/set-kth-bit.java
@@ -0,0 +1,10 @@
+// User function Template for Java
+class Solution {
+    static int setKthBit(int n, int k) {
+        int num=1<<k;
+        return (n|num);
+        
+        
+        
+    }
+}
