7c46a70a53b555a0ff12c2a745d59ead8707d38b
Harshil Sharma <harshils3107@gmail.com>
2026-04-06 16:32:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Disarrangement of balls/disarrangement-of-balls.java
@@ -0,0 +1,20 @@
+// User function Template for Java
+
+class Solution {
+    static int countDer(int n) {
+        //BY TABULATION
+        if(n==1) return 0;
+        int[] dp = new int[n+1];
+        dp[1]=0; dp[2]=1;
+        for(int i=3;i<=n;i++) {
+            dp[i]=(i-1)*(dp[i-1]+dp[i-2]);
+            
+        }
+        return dp[n];
+        
+        
+        
+        
+    }
+    
+}
