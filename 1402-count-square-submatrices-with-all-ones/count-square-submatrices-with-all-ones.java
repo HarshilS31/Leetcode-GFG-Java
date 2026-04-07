4a5a37d7d430c118df779d51c86fe07083a6c3bd
Harshil Sharma <harshils3107@gmail.com>
2026-04-07 19:55:51 +0530

    Time: 6 ms (85.98%) | Memory: 62.4 MB (6.28%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -4,11 +4,14 @@ class Solution {
         for(int i=0;i<arr.length;i++) {
             for(int j=0;j<arr[0].length;j++) {
                 if(arr[i][j]==0) continue;
-                if(i>0 && j>0) arr[i][j]+=Math.min(arr[i-1][j],Math.min(arr[i][j-1],arr[i-1][j-1]));
+                if(i>0 && j>0) arr[i][j]+=min(arr[i-1][j],arr[i][j-1],arr[i-1][j-1]);
                 count+=arr[i][j];
             }
         }
         return count;
+    }
+    public int min(int i1,int i2,int i3) {
+        return Math.min(i1,Math.min(i2,i3));
     }
     
 
