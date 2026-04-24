c71501db0cd7d911f8d58b238eec9b8cb7fab034
Harshil Sharma <harshils3107@gmail.com>
2026-04-24 21:56:31 +0530

    Time: 8 ms (17.58%) | Memory: 45.5 MB (76.38%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.java
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -7,9 +7,9 @@ class Solution {
             else if(moves.charAt(i)=='L') x--;
             else x++;
         }
-        if (x==0 && y==0) return true;
-        return false;
+        return (x==0 && y==0);
+   
 
-  
+
     }
 }
