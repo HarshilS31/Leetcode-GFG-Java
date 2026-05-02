f081f3b2841e24df32c6f40eb42a528784435055
Harshil Sharma <harshils3107@gmail.com>
2026-05-02 18:00:25 +0530

    Time: 24 ms (77.45%) | Memory: 48.6 MB (70.96%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -2,22 +2,15 @@ class Solution {
     public int subarraySum(int[] arr, int k) {
         int ans = 0;
         int currSum = 0;
-    // Map stores: <PrefixSum, HowManyTimesSeen>
         HashMap<Integer, Integer> map = new HashMap<>();
-    
-    // Crucial: A sum of 0 has been seen once (before we start)
         map.put(0, 1);
     
         for (int i = 0; i < arr.length; i++) {
             currSum += arr[i];
-            
-        // If (currSum - k) exists in map, it means there's a 
-        // subarray ending here that sums to k
             if (map.containsKey(currSum - k)) {
                 ans += map.get(currSum - k);
             }
-        
-        // Record this current sum in the map
+
             map.put(currSum, map.getOrDefault(currSum, 0) + 1);
         }
     
