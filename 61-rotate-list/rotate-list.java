a8471a28d8514d7a3c3eeb4e0a533584759a17bb
Harshil Sharma <harshils3107@gmail.com>
2026-05-05 21:16:32 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (44.03%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -1,50 +1,27 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
 class Solution {
-     public int length(ListNode head) {
-        ListNode temp=head;
-        int len=0;
-        while(temp!=null) {
-            temp=temp.next;
-            len++;
-        }
-        return len; 
- }
     public ListNode rotateRight(ListNode head, int k) {
-        if(head==null || head.next==null) return head;
-        int n=length(head);
-        k%=n;
-        if(k==0) return head;
-        ListNode s=head;
-        ListNode f=head;
-        for(int i=1;i<=k+1;i++) {
-            f=f.next;
-
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }   
+        int length = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
         }
-        while(f!=null) {
-            s=s.next;
-            f=f.next;
+        k = k % length;
+        if (k == 0) {
+            return head;
         }
-        ListNode a=s.next;
-        s.next=null;
-        ListNode tail=a;
-
-        while(tail.next!=null) {
-            tail=tail.next;
+        ListNode newTail = head;
+        for (int i = 0; i < length - k - 1; i++) {
+            newTail = newTail.next;
         }
-        tail.next=head;
-        return a;
-        }
-
+        ListNode newHead = newTail.next;
         
-    
+        newTail.next = null;
+        tail.next = head;
+        
+        return newHead;
+    }
 }
