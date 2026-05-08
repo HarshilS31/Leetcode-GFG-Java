af0f3a3ac799d820f4431f9c568e02159986103a
Harshil Sharma <harshils3107@gmail.com>
2026-05-08 15:32:03 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (57.03%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,28 +1,24 @@
 class Solution {
-    public int singleNonDuplicate(int[] arr) {
-        if(arr.length==1) return arr[0];
-        else if(arr[0]!=arr[1]) return arr[0];
-        if(arr[arr.length-1]!=arr[arr.length-2]) return arr[arr.length-1];
-        int lo=0,hi=arr.length-1,p=0,d=0;
-        while(hi>=lo) {
-            int m=lo+(hi-lo)/2,f=0,s=0;
-            if (arr[m]!=arr[m+1] && arr[m]!=arr[m-1]) return arr[m];
-            if(arr[m-1]==arr[m])  {
-                 f=m-1;
-                 s=m;
-            }
-               
-            else {
-                f=m;
-                s=m+1;
-            }
-            int dleft=f-lo;
-            int dright=hi-s;
-            if(dleft%2==0) lo=s+1;
-            else  hi=f-1;
-            
-            }
+    public int singleNonDuplicate(int[] nums) {
+        if(nums.length==1) return nums[0];
+        int lo=0;
+        int hi=nums.length-1;
+        while(hi>=lo){
+            int mid=lo+(hi-lo)/2;
+            if (mid == 0 && nums[mid] != nums[mid + 1]) return nums[mid];
+            if (mid == nums.length - 1 && nums[mid] != nums[mid - 1]) return nums[mid];
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1] && mid>0 && mid<nums.length-1) return nums[mid];
+            int first=mid;
+            int second=mid;
+            if(nums[mid-1]==nums[mid]) first=mid-1;
+            else second=mid+1;
+            int leftCount=first-lo;
+            int rightCount=hi-second;
+            if(leftCount%2==0) lo=second+1;
+            else hi=first-1;
+        }
         return -1;
 
-        }
+        
     }
+}
