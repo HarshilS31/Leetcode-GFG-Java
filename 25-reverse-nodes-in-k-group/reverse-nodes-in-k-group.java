1f9d8369975fbffa66d9d3a3a2d5945794d62852
Harshil Sharma <harshils3107@gmail.com>
2026-05-13 15:10:53 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (16.08%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -6,26 +6,34 @@ class Solution {
         ListNode prevTail = dummy;
         ListNode temp = head;
         while (temp != null) {
-            ListNode check = temp;
-            for (int i = 0; i < k; i++) {
-                if (check == null) {
-                    prevTail.next = temp;
-                    return dummy.next;
-                }
-                check = check.next;
+            ListNode[] pair = detacher(temp, k);
+            ListNode revHead = pair[0];
+            ListNode nextGrp = pair[1];
+            if (revHead == null) {
+                prevTail.next = temp;
+                break;
             }
-            ListNode prev = null;
-            ListNode curr = temp;
-            for (int i = 0; i < k; i++) {
-                ListNode nextNode = curr.next;
-                curr.next = prev;
-                prev = curr;
-                curr = nextNode;
-            }
-            prevTail.next = prev;
+            prevTail.next = revHead;
             prevTail = temp;
-            temp = check;
+            temp = nextGrp;
         }
+
         return dummy.next;
+    }
+    private ListNode[] detacher(ListNode head, int k) {
+        ListNode check = head;
+        for (int i = 0; i < k; i++) {
+            if (check == null) return new ListNode[]{null, null};
+            check = check.next;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        for (int i = 0; i < k; i++) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return new ListNode[]{prev, curr};
     }
 }
