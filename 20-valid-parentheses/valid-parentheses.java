0f1a72830b6a85862f87b16baf2b68ff9b07d446
Harshil Sharma <harshils3107@gmail.com>
2026-05-23 08:39:21 +0530

    Time: 3 ms (87.89%) | Memory: 43.3 MB (56.77%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> temp = new Stack<>();
+        if(s.length()%2!=0) return false;
+
+        for(int i=0;i<s.length();i++) {
+            char c=s.charAt(i);
+            if(c=='[' || c=='(' || c=='{') temp.push(c);
+            // else if(c==']' || c==')' || c=='}' && temp.size()==0) return false;
+            else {
+                if(temp.size()==0) return false;
+                char top=temp.peek();
+                if (counterPart(top,s.charAt(i))) {
+                    temp.pop();
+
+                }
+                else return false;
+            }
+        }
+        return (temp.size()==0);
+
+    }
+
+    boolean counterPart(char top,char c) {
+        if(top=='(' && c==')') return true;
+        else if(top=='[' && c==']') return true;
+        else if(top=='{' && c=='}') return true;
+        return false;
+
+    }
+}
