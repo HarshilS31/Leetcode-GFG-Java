852f45e6f2c685fcf713f2107d32737248c83276
Harshil Sharma <harshils3107@gmail.com>
2026-05-23 18:34:04 +0530

    Time: 1 ms (58.68%) | Memory: 48.8 MB (15.24%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+
+class Solution {
+    public int trap(int[] height) {
+        int n =height.length;
+        int[] preMax=new int[n];
+        int[] postMax=new int[n];
+        preMax[0]=height[0];
+        for(int i=1;i<n;i++) {
+            preMax[i]=Math.max(preMax[i-1],height[i]);
+
+        }
+        postMax[n-1]=height[n-1];
+        for(int i=n-2;i>=0;i--) {
+            postMax[i]=Math.max(height[i],postMax[i+1]);
+        }
+        int area=0;
+        for(int i=0;i<n;i++) {
+            if(height[i]==postMax[i] || height[i]==preMax[i]) continue;
+            else {
+                area+=Math.min(postMax[i],preMax[i])-height[i];
+            }
+
+        }
+        return area;
+        
+   }
+}
