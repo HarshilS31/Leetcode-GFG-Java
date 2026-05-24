2052cb5651e97701fbd0cd37659b6c55f88c13b7
Harshil Sharma <harshils3107@gmail.com>
2026-05-24 13:13:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (16.47%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.java
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.java
@@ -1,14 +1,10 @@
 class Solution {
     public int kthFactor(int n, int k) {
-        for(int i=1;i<3;i++) {
-            if (i==k && i==n) return n;
-        }
-        if(n<=k) return -1;
-        ArrayList<Integer> factors = new ArrayList<>();
+        int Kth=0;
         for(int i=1;i<=n;i++) {
-            if(n%i==0) factors.add(i);
+            if(n%i==0) Kth++;
+            if(Kth==k) return i;
         }
-        if(factors.size()<k) return -1;
-        else return factors.get(k-1);      
+    return -1;
     }
 }
