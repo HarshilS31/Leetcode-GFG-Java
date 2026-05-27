899af433e074d5e59df375cb7a3d3b824711d0e9
Harshil Sharma <harshils3107@gmail.com>
2026-05-27 12:12:37 +0530

    Time: 1 ms (86.91%) | Memory: 47.3 MB (56.44%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,31 +1,29 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int count = 0;
-        boolean[] visited = new boolean[isConnected.length]; 
-        
-        for (int i = 0; i < isConnected.length; i++) {
-            if (!visited[i]) { 
-                bfs(isConnected, i, visited); 
+    public int findCircleNum(int[][] adj) {
+        int count=0;
+        int n=adj.length;
+        boolean[] visited=new boolean[n];
+        for(int i=0;i<n;i++) {
+            if(!visited[i]) {
+                bfs(adj,visited,i);
                 count++;
-                
             }
         }
         return count;
     }
-    void bfs(int[][] cities, int node, boolean[] visited) {
-        visited[node] = true; 
-        int m = cities.length; 
-        int n = cities[0].length; 
-        Queue<Integer> q = new LinkedList<>(); 
-        q.add(node); 
-        while (q.size() > 0) { 
-            int front = q.remove(); 
-            for (int a = 0; a < cities.length; a++) { 
-                if (cities[front][a] == 1 && !visited[a]) { 
-                    q.add(a); 
-                    visited[a] = true; 
-                } 
-            } 
-        } 
+    void bfs(int[][] adj,boolean[] visited,int idx) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(idx);
+        while(q.size()>0) {
+            int front=q.remove();
+            for(int k=0;k<adj.length;k++) {
+                if(adj[front][k]==1 && !visited[k]) {
+                    visited[k]=true;
+                    q.add(k);
+                }
+            }   
+        }
+        
+
     }
 }
