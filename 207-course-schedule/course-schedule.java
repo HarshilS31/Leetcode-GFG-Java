7bf437dc629fdd95eab2640e7acfbd47623d7c0e
Harshil Sharma <harshils3107@gmail.com>
2026-05-29 09:31:37 +0530

    Time: 3 ms (99.73%) | Memory: 47 MB (75.95%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -8,7 +8,7 @@ class Solution {
             adj[edge[1]].add(edge[0]);
         }
 
-        // State array: 0 = unvisited, 1 = visiting, 2 = visited
+        // State array: 0 -> unvisited, 1 -> visiting, 2 -> visited
         int[] state = new int[numCourses];
         for (int i = 0; i < numCourses; i++) {
             if (state[i] == 0) {
@@ -22,17 +22,13 @@ class Solution {
 
     private boolean hasCycle(List<Integer>[] adj, int[] state, int curr) {
         state[curr] = 1;
-        for (int next : adj[curr]) {
-            if (state[next] == 1) {
-                return true;
-            }
-            if (state[next] == 0) {
-                if (hasCycle(adj, state, next)) {
-                    return true;
-                }
+        for(int next:adj[curr]) {
+            if(state[next]==1) return true;
+            else if(state[next]==0) {
+                if(hasCycle(adj,state,next)) return true;
             }
         }
-        state[curr] = 2; 
+        state[curr]=2;
         return false;
     }
 }
